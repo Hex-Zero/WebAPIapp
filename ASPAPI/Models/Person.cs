125ef7c2809cda9065bab11bee64a0b034471add
Hex-Zero <hex0@live.com>
2020-02-09 15:38:41 +0000

    Person model

---
+++ ASPAPI/Models/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ASPAPI.Models
+{
+    public class Person
+    {
+        public int Id { get; set; } = 0 ;
+        public string FirstName { get; set; } = "";
+        public string LastName { get; set; } = "";
+
+    }
+}
